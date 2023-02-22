74f6ef231914c726973f44c9378834ef283f22d0
unknown <kauetmurakami@gmail.com>
2023-02-22 18:49:16 -0300

    add name observable user

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,124 +1,142 @@
 import 'package:example/app/my_app.dart';
 import 'package:flutter/material.dart';
+import 'package:minimals_state_manager/app/provider/min_provider.dart';
+import 'package:minimals_state_manager/app/state_manager/controller/min_controller.dart';
+import 'package:minimals_state_manager/app/state_manager/extensions/min_listen.dart';
+import 'package:minimals_state_manager/app/state_manager/extensions/min_update.dart';
+import 'package:minimals_state_manager/app/widgets/minx_widget.dart';
+import 'package:minimals_state_manager/app/widgets/observable_widget.dart';
 
 import 'package:url_strategy/url_strategy.dart';
 
 // with 2.0 navigation
-void main() {
-  setPathUrlStrategy();
-  runApp(const MyApp());
-  // runApp(const MaterialApp()); //normal navigation
-}
-
-
 // void main() {
-//   runApp(MaterialApp(
-//     home: MinProvider(controller: MyController(), child: MyPage()),
-//   ));
+//   setPathUrlStrategy();
+//   runApp(const MyApp());
 //   // runApp(const MaterialApp()); //normal navigation
 // }
 
-// class MyPage extends StatelessWidget {
-//   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-//   MyPage({super.key});
+void main() {
+  runApp(MaterialApp(
+    home: MinProvider(controller: MyController(), child: MyPage()),
+  ));
+  // runApp(const MaterialApp()); //normal navigation
+}
 
-//   @override
-//   Widget build(BuildContext context) => Scaffold(
-//         floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-//         floatingActionButton: MinX<MyController>(
-//           builder: (context, controller) => Column(
-//             mainAxisAlignment: MainAxisAlignment.end,
-//             children: [
-//               FloatingActionButton(
-//                 backgroundColor: Colors.lightGreen,
-//                 onPressed: () => controller.increment(),
-//                 child: const Icon(Icons.add),
-//               ),
-//               const SizedBox(
-//                 height: 16.0,
-//               ),
-//               FloatingActionButton(
-//                 backgroundColor: Colors.redAccent,
-//                 onPressed: () => controller.decrement(),
-//                 child: const Icon(Icons.remove),
-//               ),
-//               const SizedBox(
-//                 height: 16.0,
-//               ),
-//               FloatingActionButton(
-//                 onPressed: () => controller.refresh(),
-//                 child: const Icon(Icons.refresh),
-//               ),
-//             ],
-//           ),
-//         ),
-//         body: SafeArea(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               const Text('check out the full example in the example folder'),
-//               Container(
-//                 margin: const EdgeInsets.only(
-//                     left: 32.0, right: 32.0, bottom: 32.0),
-//                 child: Form(
-//                   key: formKey,
-//                   child: MinX<MyController>(
-//                     builder: (context, controller) => TextFormField(
-//                       onChanged: (value) => controller.onChangedName(value),
-//                       validator: (value) => controller.validateName(value),
-//                       onSaved: (newValue) => controller.onSaveName(newValue),
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//               Column(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 children: [
-//                   Row(
-//                     mainAxisAlignment: MainAxisAlignment.center,
-//                     children: [
-//                       MinX<MyController>(
-//                         builder: (context, controller) => $(
-//                           (count) => Text(
-//                             'Count $count',
-//                             style: const TextStyle(
-//                               fontSize: 24.0,
-//                               fontWeight: FontWeight.w600,
-//                             ),
-//                           ),
-//                           listener: controller.count,
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ],
-//               ),
-//             ],
-//           ),
-//         ),
-//       );
-// }
+class MyPage extends StatelessWidget {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  MyPage({super.key});
 
-// class MyController extends MinController {
-//   @override
-//   void onInit() {
-//     print('init controller');
-//     super.onInit();
-//   }
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        floatingActionButton: MinX<MyController>(
+          builder: (context, controller) => Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FloatingActionButton(
+                backgroundColor: Colors.lightGreen,
+                onPressed: () => controller.increment(),
+                child: const Icon(Icons.add),
+              ),
+              const SizedBox(
+                height: 16.0,
+              ),
+              FloatingActionButton(
+                backgroundColor: Colors.redAccent,
+                onPressed: () => controller.decrement(),
+                child: const Icon(Icons.remove),
+              ),
+              const SizedBox(
+                height: 16.0,
+              ),
+              FloatingActionButton(
+                onPressed: () => controller.refresh(),
+                child: const Icon(Icons.refresh),
+              ),
+            ],
+          ),
+        ),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('check out the full example in the example folder'),
+              Container(
+                margin: const EdgeInsets.only(
+                    left: 32.0, right: 32.0, bottom: 32.0),
+                child: Form(
+                  key: formKey,
+                  child: MinX<MyController>(
+                    builder: (context, controller) => Column(
+                      children: [
+                        $(
+                          (user) => Text('${user.name}'),
+                          listener: controller.user,
+                        ),
+                        TextFormField(
+                          onChanged: (value) => controller.onChangedName(value),
+                          validator: (value) => controller.validateName(value),
+                          onSaved: (newValue) =>
+                              controller.onSaveName(newValue),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      MinX<MyController>(
+                        builder: (context, controller) => $(
+                          (count) => Text(
+                            'Count $count',
+                            style: const TextStyle(
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          listener: controller.count,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+}
 
-//   final count = 0.minx;
-//   ValueNotifier<User> user = User().minx;
+class MyController extends MinController {
+  @override
+  void onInit() {
+    print('init controller');
+    super.onInit();
+  }
 
-//   increment() => count.value++;
-//   decrement() => count.value--;
-//   refresh() => count.value = 0;
+  final count = 0.minx;
+  ValueNotifier<User> user = User().minx;
 
-//   onChangedName(str) => user.update((val) => val.name = str);
-//   onSaveName(str) => user.update((val) => val.name = str);
-//   validateName(str) => str.length < 2 ? 'insert valid name' : null;
-// }
+  increment() => count.value++;
+  decrement() => count.value--;
+  refresh() => count.value = 0;
 
-// class User {
-//   User({this.name});
-//   String? name;
-// }
+  onChangedName(str) {
+    user.update((val) => val.name = str);
+    print(user.value.name);
+  }
+
+  onSaveName(str) => user.update((val) => val.name = str);
+  validateName(str) => str.length < 2 ? 'insert valid name' : null;
+}
+
+class User {
+  User({this.name});
+  String? name;
+}
--- lib/app/state_manager/extensions/min_update.dart
+++ lib/app/state_manager/extensions/min_update.dart
@@ -1,20 +1,11 @@
-import 'package:flutter/src/foundation/change_notifier.dart';
-
-// class MinExtensions with ChangeNotifier {
-//   void update(Function(T) update) {
-//     T value = this.value;
-//     update(value);
-//     this.value = value;
-//     notifyListeners();
-//   }
-// }
-//abstrair metodo para que possamos usar o change notifier
+import 'package:flutter/foundation.dart';
 
 /// The ValueNotifierUpdater extension provides an update method for ValueNotifier objects,
 /// allowing you to update their values while notifying listeners.
 /// The extension method accepts a function that takes the current value of the ValueNotifier and
 /// returns a new value. It updates the ValueNotifier with the new value, notifies its
 /// listeners, and keeps the reference to the same instance of the ValueNotifier.
+
 extension ValueNotifierUpdater<T> on ValueNotifier<T> {
   void update(Function(T) update) {
     T value = this.value;
@@ -23,3 +14,22 @@ extension ValueNotifierUpdater<T> on ValueNotifier<T> {
     notifyListeners();
   }
 }
+
+
+// abstrair notifylisteners
+// class MinUpdater<T> extends ChangeNotifier {
+//   ValueNotifier<T> _notifier;
+
+//   MinUpdater(this._notifier);
+
+//   void update(Function(T) update) {
+//     T value = _notifier.value;
+//     update(value);
+//     _notifier.value = value;
+//     notifyListeners();
+//   }
+// }
+
+// extension ValueNotifierUpdater<T> on ValueNotifier<T> {
+//   void update(Function(T) update) => MinUpdater<T>(this).update(update);
+// }

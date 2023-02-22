c157454ca0bc0c6bc855ec732911c7700b020caf
Kaue Murakami <kauetmurakami@gmail.com>
2023-02-22 15:48:18 -0300

    feat:  add plataforms tests

--- lib/app/state_manager/extensions/min_update.dart
+++ lib/app/state_manager/extensions/min_update.dart
@@ -1,12 +1,20 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/src/foundation/change_notifier.dart';
 
-/**
- * The ValueNotifierUpdater extension provides an update method for ValueNotifier objects,
- * allowing you to update their values while notifying listeners. 
- * The extension method accepts a function that takes the current value of the ValueNotifier and 
- * returns a new value. It updates the ValueNotifier with the new value, notifies its 
- * listeners, and keeps the reference to the same instance of the ValueNotifier. 
- */
+// class MinExtensions with ChangeNotifier {
+//   void update(Function(T) update) {
+//     T value = this.value;
+//     update(value);
+//     this.value = value;
+//     notifyListeners();
+//   }
+// }
+//abstrair metodo para que possamos usar o change notifier
+
+/// The ValueNotifierUpdater extension provides an update method for ValueNotifier objects,
+/// allowing you to update their values while notifying listeners.
+/// The extension method accepts a function that takes the current value of the ValueNotifier and
+/// returns a new value. It updates the ValueNotifier with the new value, notifies its
+/// listeners, and keeps the reference to the same instance of the ValueNotifier.
 extension ValueNotifierUpdater<T> on ValueNotifier<T> {
   void update(Function(T) update) {
     T value = this.value;

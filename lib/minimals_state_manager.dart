c00bc658943716cf8f57721bd1d3d49e10857b5f
Kaue Murakami <kmurakami-ext@tendaatacado.com.br>
2026-06-12 11:39:20 -0300

    Add exports new files

--- lib/minimals_state_manager.dart
+++ lib/minimals_state_manager.dart
@@ -2,21 +2,17 @@ library minimals_state_manager;
 
 // lib/min_state.dart
 
-// 1. Exporte o seu Provider e Notifier
 export 'app/provider/min_provider.dart';
 export 'app/provider/min_multi_provider.dart';
 export 'app/state/min_notifier.dart';
 
-// 2. Exporte o seu Widget de alta performance $
-export 'app/widgets/min_selector.dart'; // O seu class $
+export 'app/widgets/min_selector.dart';
 
-// 3. EXPORTE AS SUAS EXTENSÕES AQUI!
-export 'app/extensions/min_provider_extensions.dart'; // Onde estão o .read e .watch
+export 'app/extensions/min_provider_extensions.dart';
 export 'app/state_manager/extensions/min_update.dart';
 export 'app/state_manager/service/min_service.dart';
-export 'app/state/mixins/min_app_lifecycle.dart';
+export 'app/observers/min_app_lifecycle.dart';
 export 'app/types/min_record.dart';
 export 'app/state/min_props.dart';
 
-//minimal state manager main
 class Min {}
